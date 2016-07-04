1d13f63794f09ad4f9e72d26405c4a3796f3a061
wll126 <yongyuanaiwan@126.com>
2016-07-04 23:01:20 +0800

    Update UntitledClass.java
    wo ti jiao le

--- untitled/src/UntitledClass.java
+++ untitled/src/UntitledClass.java
@@ -4,5 +4,7 @@
 public class UntitledClass {
     public static void main(String[] args) {
         System.out.println("Thank you very much!");
+        //助手
+        System.err.println("煮熟了，吃饱");
     }
 }
