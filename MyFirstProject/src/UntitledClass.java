fa32af88f066556852dabadafde04d0438a22330
wll126 <yongyuanaiwan@126.com>
2016-07-04 23:09:01 +0800

    Untified e l

--- MyFirstProject/src/UntitledClass.java
+++ MyFirstProject/src/UntitledClass.java
@@ -6,6 +6,6 @@ public class UntitledClass {
         System.out.println("Thank you very much!");
         //助手
         System.err.println("煮熟了，吃饱");
-        System.out.println("我饿了！！！");
+        System.out.println("我饿了！！！");      //
     }
 }
